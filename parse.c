23bb794ec6144849e5fa9e98b697f399f7e545e5
Carl Aiau <carlaiau@Carls-MacBook-Pro.local>
2018-04-05 13:10:01 +1200

    Date sorted on parse

--- parse.c
+++ parse.c
@@ -5,204 +5,121 @@
 
 #define BUFFER_SIZE 10000
 
-int not_tag(char* token);
-char* clean_up_token(char* token);
-char* clean_up_docid(char* token);
-char* clean_up_date(char* token);
-
+void output_id(char *token);
+void output_date(char *token);
+void output_clean(char *token);
 
 int main(int argc, char *argv []) {
+	int id, date = 0; /* expecting id, date bool */
 	FILE *file_handle;
-	const char *new_line_char_delim = " $.,\"\'\n\r\t();:{}+^#@*";
+	const char *delim = " $.,\"\'\n();:{}+^#@*";
 	char buffer[BUFFER_SIZE];
 	file_handle = fopen(argv[1], "r");
+
+
     if (!file_handle) {
         fprintf(stderr, "Unable to open input!\n");
         return 1;
     }
-    /* read each line into the buffer */
+	
+	/* read each line into the buffer */
     while(fgets( buffer, BUFFER_SIZE, file_handle) != NULL){
     	/* splits based on delimitering strings */
-    	char *token = strtok(buffer, new_line_char_delim);		
-		while(token != NULL){
-			/* 
-			 * New Doc! 
-			 * Output a blank line
-			 */
-			if(strcmp(token, "<DOC>") == 0) { /* This is a new doc */
-				printf("\n");
+    	char *token = strtok(buffer, delim);		
+		while(token != NULL) {
+            if(id == 1) {
+				output_id(token);
+                id = 0;
+            }
+			if(date == 1){
+				output_date(token);
+				date = 0;
 			}
-
-			/* 
-			 * Doc ID
-			 * Skip inital line and grab Doc ID, and clean it.
-			 */
-			else if(strcmp(token, "<DOCNO>") == 0){
-				printf( "%s\n", token);
-				token = strtok(NULL, new_line_char_delim); // Grab the next token
-				printf( "Whats this? %s\n", token);
-				if(token != NULL){
-					token = clean_up_docid(token);
-					printf( "%s\n", token);
+			/* Is a Tag */
+            else if(token[0] == '<'){
+				/* Next Token will be ID */
+				if(strcmp(token, "<DOCNO>") == 0) {
+                    id = 1;
+                }
+				/* Next Token will be Date */
+				if(strcmp(token, "<DATE>") == 0 ||
+					strcmp(token, "<DD>") == 0){
+					date = 1;
 				}
-				
+            }
+			/* This is normal content */
+			else{
+				output_clean(token);
 			}
-
-			/* 
-			 * This is Date tag
-			 * Needs cleaned to ensure only / or number
-		 	 *
-			 */
-			else if(strcmp(token, "<DD>") == 0){ 
-					
-				token = strtok(NULL, new_line_char_delim); // Grab the next Line
-				
-				token = clean_up_date(token);
-				if(strlen(token) > 0){
-					printf( "%s\n", token);
-				}
-				
-			}
-
-
-			/* 
-			 * all other tokens
-			 */
-			else if( not_tag(token) ) {
-				token = clean_up_token(token);
-				if(strlen(token) > 1){
-					//printf( "%s\n", token);
-				}
-			}
-
-			token = strtok(NULL, new_line_char_delim);
-			
+			token = strtok(NULL, delim);
 		}
-    }
-    // closing files.
+	}
+	
     fclose(file_handle);  
-    return 0;
+	return 0;
 }
 
-/* Easy removal of tags that are on seperate lines */
-int not_tag(char* token){
-	if(strcmp(token, "</DOC>") == 0){
-		return 0;
+void output_id(char* token){
+	unsigned int i;
+	unsigned int len = strlen(token);
+	if(len > 0){
+		printf("\n");
 	}
-	if(strcmp(token, "<DOCNO>") == 0){
-		return 0;
-	}
-	if(strcmp(token, "</DOCNO>") == 0){
-		return 0;
-	}	
-	if(strcmp(token, "<HL>") == 0){
-		return 0;
-	}
-	if(strcmp(token, "<SO>") == 0){ 
-		return 0;
-	}
-	if(strcmp(token, "</SO>") == 0){ 
-		return 0;
-	}	
-	if(strcmp(token, "<LP>") == 0){ 
-		return 0;
-	}		
-	if(strcmp(token, "</LP>") == 0){ 
-		return 0;
-	}	
-	if(strcmp(token, "</HL>") == 0){
-		return 0;
-	}		
-	if(strcmp(token, "<IN>") == 0){
-		return 0;
-	}
-	if(strcmp(token, "</IN>") == 0){
-		return 0;
-	}
-	if(strcmp(token, "<DATELINE>") == 0){
-		return 0;
-	}	
-	if(strcmp(token, "</DATELINE>") == 0){
-		return 0;
-	}	
-	if(strcmp(token, "<TEXT>") == 0){
-		return 0;
-	}
-	if(strcmp(token, "</TEXT>") == 0){
-		return 0;
-	}
-	if(strcmp(token, "<DOCID>") == 0){		
-		return 0;
-	}
-	if(strcmp(token, "</DOCID>") == 0){		
-		return 0;
-	}	
-	if(strcmp(token, "<CO>") == 0){ 
-		return 0;
-	}
-	if(strcmp(token, "</CO>") == 0){ 
-		return 0;
-	}				
-	return 1;
-}
-
-
-// Removes the xml tag at the end of a token.
-char* clean_up_token(char* token) {
-    char* temp_token = malloc( strlen(token) * sizeof(temp_token[0]) + 1 );
-    unsigned long inner_i = 0;
-    unsigned long i;
-    for (i = 0; i < strlen(token); i++){
-    	if(isalpha(token[i])){
-    		temp_token[inner_i++] = tolower(token[i]);
-    	}
-    	/* Strip out the & */
-    	else if(token[i] == '&'){
-    		i += 4;
-    	}
-    }
-    temp_token[inner_i] = '\0';
-    return temp_token;
-}
-
-char* clean_up_date(char* token){
-	char* temp_token = malloc( strlen(token) * sizeof(temp_token[0]) + 1 );
-    unsigned long inner_i = 0;
-    unsigned long i;
-    for (i = 0; i < strlen(token); i++){
-		if(token[i] == '<'){ // Found end tag
-			break;
+	for(i = 0; i < len && token[i] != '<'; i++){
+		if(isdigit(token[i])){
+			printf("%c", token[i]);
 		}
-    	else if(isdigit(token[i]) || token[i] == '/' ){
-    		temp_token[inner_i++] = tolower(token[i]);
-    	}
-    }
-    temp_token[inner_i] = '\0';
-    return temp_token;
+	}
+	printf("\n%s\n", token);
 }
 
-/* 
- * Use Doc ID as number index
- * but also output the full WSJ ID string for exact searching 
- */
-char* clean_up_docid(char* token){
-	printf("Token length: %d", strlen(token) );
-	char *temp_token = malloc( strlen(token) * 2 * sizeof(temp_token[0]) + 1 );
-	unsigned long i;
-	unsigned long inner_i = 0;
-
-	for(i = 0; i < strlen(token); i++){
-		if( isdigit(token[i] ) ){
+/* Attempt to preverse date in data */
+void output_date(char* token){
+	unsigned int i;
+	unsigned int inner_i = 0;
+	unsigned int len = strlen(token);
+	char* temp_token = malloc( len * sizeof(temp_token[0]) + 1 );
+	for(i = 0; i < len && token[i] != '<'; i++){
+		if(isdigit(token[i]) || token[i] == '/'){
 			temp_token[inner_i++] = token[i];
 		}
 	}
-	/* Output a new line at end of ID */
-	temp_token[inner_i++] = '\n';
+	temp_token[inner_i] = '\0';
+	if(temp_token[0] != '\0'){
+		printf("%s\n", temp_token);
+	}
+	free(temp_token);
+}
 
-	/* Repeat the DocID completely */
-	for(i = 0; i < strlen(token); i++){
-		temp_token[inner_i++] = token[i];
+void output_clean(char *token){
+	unsigned int i;
+	unsigned int inner_i = 0;
+	/* local var rather than double look */
+	unsigned int len = strlen(token);
+	char* temp_token = malloc( len * sizeof(temp_token[0]) + 1 );
+
+	/* Make sure token is not an & */	
+	if(strcmp(token, "&amp") != 0 && len > 1){
+		/* we have a tag to remove */
+		if(token[len - 1] == '>'){ 
+			for(i = 0; token[i] != '<'; i++){
+				if(isalpha(token[i])){
+					temp_token[inner_i++] =  tolower(token[i]);
+				}
+			}
+		}
+		else{
+			/* Remove Non Alphabetical */
+			for(i = 0; i < len; i++){
+				if(isalpha(token[i])){
+					temp_token[inner_i++] =  tolower(token[i]);
+				}
+			}
+		}
 	}
 	temp_token[inner_i] = '\0';
-	return temp_token;
+	if(temp_token[0] != '\0'){
+		printf("%s\n", temp_token);
+	}
+	free(temp_token);
 }
